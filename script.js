0a93d7a307283844a960c6538ef96d31355f0939
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-28 20:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+async function displayMessageWithDelay() {
+  const textInput = document.getElementById('text').value;
+  const delayInput = document.getElementById('delay').value;
+
+  // Convert delay to milliseconds
+  const delayMilliseconds = delayInput * 1000;
+
+  // Wait for the specified delay
+  await new Promise(resolve => setTimeout(resolve, delayMilliseconds));
+
+  // Display the message on the webpage
+  document.getElementById('output').innerText = textInput;
+}
+
+// Event listener for the button click
+document.getElementById('btn').addEventListener('click', async () => {
+  await displayMessageWithDelay();
+});
